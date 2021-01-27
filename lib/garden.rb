8ec58598b47daf0a474c54cf90f565494841a3a9
Fay Vera <fvera.cavalcante@gmail.com>
2021-01-27 16:22:25 -0500

    commit tree b3b63406b3ffee9a820d68a5b784dd57ebcaf6a3

--- lib/garden.rb
+++ lib/garden.rb
@@ -1,5 +1,4 @@
-# Something is missing here
-
+require_relative './plant.rb' 
 class Garden
   attr_accessor :name
 
@@ -7,19 +6,19 @@ class Garden
     @name = name
   end
 
-  def plants
-    Plant.all.select do |plant|
-      plant.garden == self
-    end
-  end
-end
+#   def plants
+#     Plant.all.select do |plant|
+#       plant.garden == self
+#     end
+#   end
+# end
 
-lawn = Garden.new(name: 'Front Lawn')
+# lawn = Garden.new(name: 'Front Lawn')
+ 
+# basil = Plant.new(name: 'Basil')
+# basil.garden = lawn
 
-basil = Plant.new(name: 'Basil')
-basil.garden = lawn
+# cucumber = Plant.new(name: 'Cucumber')
+# cucumber.garden = lawn
 
-cucumber = Plant.new(name: 'Cucumber')
-cucumber.garden = lawn
-
-p lawn.plants
+# p lawn.plants
